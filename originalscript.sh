ae8669940290375b231e2ad1c5b0e55e828ab286
Kota-Jaya-Krishna <jayakrishnakota23@gmail.com>
2025-01-09 16:11:25 +0530

    creating script

--- originalscript.sh
+++ originalscript.sh
@@ -19,7 +19,7 @@ check_status() {
            sleep 20
            echo "Starting '$SERVICE_NAME'..." 
            nohup $(systemctl start nginx) > "$LOG_FILE" 2>&1 &
-                if [ -f "$PID_FILE"]
+                if [ -f "$PID_FILE" ]
                     then
                     echo "Service '$SERVICE_NAME' started with PID $(cat $PID_FILE)."
                     exit 0
