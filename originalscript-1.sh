9d73be7f5b54624967ab506bed788cfb4a7709f3
Kota-Jaya-Krishna <jayakrishnakota23@gmail.com>
2025-01-10 15:59:10 +0530

    creating script

--- originalscript-1.sh
+++ originalscript-1.sh
@@ -3,13 +3,13 @@
 SERVICE_NAME="nginx"
 
 check_status() {
-    SERVICE_PID1="$(pgrep -f "$SERVICE_NAME")"
-    if [ ! -z "$SERVICE_PID1" ]
+    SERVICE_PID="$(pgrep -f "$SERVICE_NAME")"
+    if [ ! -z "$SERVICE_PID" ]
        then
-           echo "Service '$SERVICE_NAME' is already running with PID '$SERVICE_PID1'."
+           echo "Service '$SERVICE_NAME' is already running with PID '$SERVICE_PID'."
            echo "Stopping '$SERVICE_NAME'..."
            # Stop all PIDs associated with the service
-           for PID in $SERVICE_PID1;do
+           for PID in $SERVICE_PID;do
            kill -9 "$PID"
            done
                 if [ "$?" -eq 0 ]
@@ -20,26 +20,26 @@ check_status() {
            echo "Starting '$SERVICE_NAME'..." 
            systemctl start "$SERVICE_NAME"
            sleep 10
-           SERVICE_PID1="$(pgrep -f "$SERVICE_NAME")"
-                if [ -z "$SERVICE_PID1" ]
+           SERVICE_PID="$(pgrep -f "$SERVICE_NAME")"
+                if [ -z "$SERVICE_PID" ]
                     then
                         echo "Service '$SERVICE_NAME' is not started Successfully."
-                        exit 0
-                    else
-                        echo "Service '$SERVICE_NAME' started with PID '$SERVICE_PID1'."
                         exit 1
+                    else
+                        echo "Service '$SERVICE_NAME' started with PID '$SERVICE_PID'."
+                        exit 0
                 fi
     else
         echo "Starting '$SERVICE_NAME'..." 
         systemctl start "$SERVICE_NAME"
-        SERVICE_PID1="$(pgrep -f "$SERVICE_NAME")"
+        SERVICE_PID="$(pgrep -f "$SERVICE_NAME")"
         sleep 10
-                if [ -z "$SERVICE_PID1" ]
+                if [ -z "$SERVICE_PID" ]
                     then
                         echo "Service '$SERVICE_NAME' is not started Successfully."
                         exit 1
                     else
-                        echo "Service '$SERVICE_NAME' started with PID '$SERVICE_PID1'."
+                        echo "Service '$SERVICE_NAME' started with PID '$SERVICE_PID'."
                         exit 0
                 fi
     fi
